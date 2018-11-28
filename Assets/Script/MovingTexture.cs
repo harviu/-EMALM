7401a174389d1559280d2505940da6e0425346fa
Harvey Li <aide0740@gmail.com>
2018-11-28 16:54:43 -0500

    holes and wall

--- Assets/Script/MovingTexture.cs
+++ Assets/Script/MovingTexture.cs
@@ -6,8 +6,8 @@ public class MovingTexture : MonoBehaviour {
 	public float shuffleInterval = 5f;
     public float speedP = 0.3f;
     public float tileXP = 0.4f;
-    public float rat = 1;
 
+    private float rat;
     float lastShuffle;
     float offsetX;
     float offsetY;
@@ -17,29 +17,39 @@ public class MovingTexture : MonoBehaviour {
 
     void Start()
     {
+        //get the dimension of the plane
+        rat = transform.localScale.x/ transform.localScale.z;
+
+
         r = GetComponent<Renderer>();
         lastShuffle = Time.time;
 
-        lastShuffle = Time.time;
-
+        //random starting point
         offsetX = Random.Range(0f, 1f);
         offsetY = Random.Range(0f, 1f);
-        scollY = Random.Range(0f, speedP);
 
-        tileX = Random.Range(0.2f, tileXP);
-        tileY = tileX/rat;
+        //random speed
+        //scollY = Random.Range(0f, speedP);
+        scollY = speedP;
+
+        //random tile
+        //tileX = Random.Range(0.2f, tileXP);
+        tileX = tileXP;
+        tileY = tileX/rat*16f/9f;
 
         r.material.mainTextureScale = new Vector2(tileX, tileY);
         //Debug.Log(scollY);
     }
 	// Update is called once per frame
 	void Update () {
-        if (Time.time - lastShuffle > 5)
+        //shuffle speed
+        /*
+        if (Time.time - lastShuffle > shuffleInterval)
         {
             lastShuffle = Time.time;
             
             scollY = Random.Range(0f, speedP);
-        }
+        }*/
 
 		offsetY += Time.deltaTime *scollY;
 		r.material.mainTextureOffset = new Vector2 (offsetX, offsetY);
